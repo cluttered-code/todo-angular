28c4637805ede55f0f6a1df672de5cabd4caf7e4
David Clutter <cluttered.code@gmail.com>
2016-01-14 12:32:30 -0700

    basic setup always pointing at dev

--- public/js/controllers/todo-index-controller.js
+++ public/js/controllers/todo-index-controller.js
@@ -1,4 +1,4 @@
 angular.module("TodoApp")
     .controller("TodoIndexController", function($scope, $routeParams, Todo) {
-        $scope.todos = [{name: 'foo'}]; //Todo.query();
+        $scope.todos = Todo.query();
 });
--- public/js/services/todo.js
+++ public/js/services/todo.js
@@ -1,6 +1,5 @@
 angular.module("TodoApp")
     .factory('Todo', function NoteFactory($resource) {
-        $http.defaults.useXDomain = true
         return $resource("http://todo-dev.clutterclan.com/todo/:id", {}, {
             update: {
                 method: "PUT"
